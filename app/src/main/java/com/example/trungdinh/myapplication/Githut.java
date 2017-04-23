06c99c035530bca8358661066048f122620efe13
TrungDinh <dinhtrum@gmail.com>
2017-04-24 00:06:50 +0700

    SuaGit

--- app/src/main/java/com/example/trungdinh/myapplication/Githut.java
+++ app/src/main/java/com/example/trungdinh/myapplication/Githut.java
@@ -1,8 +1,10 @@
 package com.example.trungdinh.myapplication;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by TrungDinh on 4/23/2017.
  */
 
-public class Githut {
+public class Githut extends AppCompatActivity {
 }
